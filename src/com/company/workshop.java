c600e2ccd3ca427923274c9476152340618b961c
AbikMushyakho <abik.mushyakho94@gmail.com>
2021-05-03 09:02:10 +0545

    workshop created

--- src/com/company/workshop.java
+++ src/com/company/workshop.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class workshop {
 
 
+
+    public static void main(String[] args) {
+
+        System.out.println("hello World");
+    }
 
 
 }
