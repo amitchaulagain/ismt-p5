d114f2663d550f0402c4d3e31360b71964abd458
AbikMushyakho <abik.mushyakho94@gmail.com>
2021-05-04 08:38:14 +0545

    created git ignore and changes in person

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -12,14 +12,38 @@ public class Person extends Mammal {
     private int age;
 
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     Person(){
         super("apppppp");
     }
 
 
-
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
 
 
 
